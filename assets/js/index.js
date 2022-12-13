a805b6b9772f2c6804c9e67faf908a5e641ce377
Sulaiman Amjad <sulaiman.amjad@gmail.com>
2022-12-13 10:48:34 +0000

    added questions to local storage

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,21 @@
+questions = [{
+    title: "Which method could you use to select a HTML element?",
+    options: ["getElementbyId()",
+    "selectElement()",
+    "use()",
+    "select()"],
+    correct_option_index: 0
+}]
+
 start_button = document.getElementById("start")
-
-
 let time_display = document.getElementById("time")
+let questions_div = document.getElementById("questions")
 
 function startGame(){
     console.log("start game")
     time_display.innerHTML = 60
     startTimer()
+    questions_div.classList.remove("hide")
 }
 
 start_button.onclick = function(){startGame()}
@@ -15,4 +24,11 @@ function startTimer(){
     setInterval(function(){
         time_display.innerHTML --
     }, 1000)
+}
+
+function checkAnswer(question_num, answer){
+    if (questions[question_num].correct_option_index == answer){
+        return true
+    }
+
 }
